7672d342555341d6b6e6ba71167740b83c357194
devanych <mail@devanych.ru>
2020-06-22 15:54:29 +0300

    Add ServerNormalizerInterface

---
+++ src/ServerNormalizerInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HttpSoft\Request;
+
+use Psr\Http\Message\UriInterface;
+
+interface ServerNormalizerInterface
+{
+    /**
+     * Returns a normalized request method from server parameters.
+     *
+     * @param array $server if PHP SAPI is used, it is $_SERVER.
+     * @return string request method.
+     */
+    public function normalizeMethod(array $server): string;
+
+    /**
+     * Returns a normalized protocol version from server parameters.
+     *
+     * @param array $server if PHP SAPI is used, it is $_SERVER.
+     * @return string protocol version.
+     */
+    public function normalizeProtocolVersion(array $server): string;
+
+    /**
+     * Returns a normalized request URI from server parameters.
+     *
+     * @param array $server if PHP SAPI is used, it is $_SERVER.
+     * @return UriInterface instance.
+     */
+    public function normalizeUri(array $server): UriInterface;
+
+    /**
+     * Returns a primary normalized headers from server parameters.
+     *
+     * @param array $server if PHP SAPI is used, it is $_SERVER.
+     * @return array `name => value` header pairs.
+     */
+    public function normalizeHeaders(array $server): array;
+}
